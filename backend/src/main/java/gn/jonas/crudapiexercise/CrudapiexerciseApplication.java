1edc831206bc463436d5e7a4820e26cfe2c846cd
jonasGN <j.goncalvesneto@outlook.com>
2021-09-19 18:15:21 -0300

    Initial project

---
+++ backend/src/main/java/gn/jonas/crudapiexercise/CrudapiexerciseApplication.java
@@ -0,0 +1,13 @@
+package gn.jonas.crudapiexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudapiexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudapiexerciseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/gn/jonas/crudapiexercise/CrudapiexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package gn.jonas.crudapiexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudapiexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

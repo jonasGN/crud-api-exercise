3f18b492ae8de111bb97ae46fffd830dafdeb085
jonasGN <j.goncalvesneto@outlook.com>
2021-09-19 18:52:51 -0300

    Client add and database seed

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO client (name, cpf, income, children, birth_date) VALUES ('José Cardoso', '07515934000', 3000, 2, TIMESTAMP WITH TIME ZONE '1993-03-14T21:38:43.00000Z');
+INSERT INTO client (name, cpf, income, children, birth_date) VALUES ('Maria Gabriela', '91186826002', 4366, 3, TIMESTAMP WITH TIME ZONE '1978-12-23T00:43:15.00000Z');
+INSERT INTO client (name, cpf, income, children, birth_date) VALUES ('Carla de Moraes', '39089578005', 2895.46, 0, TIMESTAMP WITH TIME ZONE '1997-10-04T05:22:22.00000Z');
+INSERT INTO client (name, cpf, income, children, birth_date) VALUES ('Pedro Gonçalves', '11891292056', 7899, 1, TIMESTAMP WITH TIME ZONE '1989-02-02T11:30:22.00000Z');
+INSERT INTO client (name, cpf, income, children, birth_date) VALUES ('Francisca Maria', '22078572047', 8866.5, 3, TIMESTAMP WITH TIME ZONE '1965-11-09T06:30:56.00000Z');
+INSERT INTO client (name, cpf, income, children, birth_date) VALUES ('Geraldo Pascoal', '69070056038', 7666.3, 3, TIMESTAMP WITH TIME ZONE '1964-02-02T12:34:00.00000Z');
+INSERT INTO client (name, cpf, income, children, birth_date) VALUES ('Alan Turing', '79462313008', 10333.9, 0, TIMESTAMP WITH TIME ZONE '1954-06-23T12:00:00.00000Z');
+INSERT INTO client (name, cpf, income, children, birth_date) VALUES ('Maicom Malaquias', '74250688003', 4000, 1, TIMESTAMP WITH TIME ZONE '1991-08-04T12:00:00.00000Z');
+INSERT INTO client (name, cpf, income, children, birth_date) VALUES ('Gabriela Marques', '70352795042', 6789, 0, TIMESTAMP WITH TIME ZONE '1995-04-10T12:00:00.00000Z');
+INSERT INTO client (name, cpf, income, children, birth_date) VALUES ('Joana Cortês', '27694750022', 7999, 2, TIMESTAMP WITH TIME ZONE '1989-07-30T04:45:00.00000Z');
+
